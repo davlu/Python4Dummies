96b484a20e64e5a34e566e7b10445f9132ae2988
たつき <hydra3301@protonmail.com>
2017-12-11 08:05:58 -0200

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -1,38 +1,40 @@
-#Hello today we are going to be coding in Python programming language, if you do not have Python latest version installed please go to https://www.python.org/ and install Python 3.6.2
-#-----------------------------------------------------------------------------
+# Hello today we are going to be coding in Python programming language, if you do not have Python latest version installed #please go to https://www.python.org/ and install Python 3.6.2
+# -----------------------------------------------------------------------------
+# or you can use a list of import just by doing this: import math, time, os, sys#
+# The print function is a function to output data to your console/terminal, to print a message put it inside of () and inside the () use quotations marks to print out text or numbers, never write the string to print outside of () or "", never type print inside the () or quotation marks
 
-#Now let's use the "print" function, the print function is pretty straight forward right? it prints the writing on the screen!
+print("This is a print message")
 
-print ("This is a print message")
-
-#Now let's try using a string to print out something, a string is a string of characters to characterize your string you just write any name in our case x = "your string here"
-
+# Now let's try using a string to print out something, a string is a string of characters to characterize your string you just write any name in our case x = "your string here"
+# In computer programming, a variable or scalar is a storage location paired with an associated symbolic name (an identifier), which contains some known or unknown quantity of information referred to as a value. 
+# In computer programming, a string is traditionally a sequence of characters, either as a literal constant or as some kind of variable. The latter may allow its elements to be mutated and the length changed, or it may be fixed (after creation). A string is generally understood as a data type and is often implemented as an array data structure of bytes (or words) that stores a sequence of elements, typically characters, using some character encoding. A string may also denote more general arrays or other sequence (or list) data types and structures.
+# Note: never type the string or integer on the left side of = and never type the variable name in the right side of =
 x = "This is a string"
 
-#To invoke strings you just type x or the name you gave to the string
+# To invoke strings you just type the name of the variable which in this case is x
 
-print x
+print (x)
 
-#We can also print numbers by using integers, to do that you replace the quotation mark and text for a number like below
+# We can also print numbers by using integers, to do that you replace the quotation mark and text for a number like below inside the variable
 
 y = 1
 
-#To invoke the integer you can do the same you did with the string 
+# To invoke the integer you can do the same with the string variable
 
-print y
+print(y)
 
-#You can combine strings and integers simply by adding both together
+# You can combine strings and integers variables simply by adding both together
 
-print x + y
+print(x + y)
 
-#Now let's learn some basic math using the addition function and print function, we will use the letter z and it is as simple as this:
+# Now let's learn some basic math using the math function and print module, we will use the letter z as a name variable and it is as simple as this:
 
-z = 24+12
+z =24+12
 
-#To invoke you do the same as the previous ones
+# To invoke you do the same as the previous ones
 
-print z
+print(z) 
 
-#Or you can just do it manually
-
-print (24+12)
+# Or you can just do it manually by using the print function, remember everytime you use the print function put the print information in ()
+# Remeber that numbers DO NOT require a quotation mark to be added together but do require when you want to print them out
+print(24+12)
